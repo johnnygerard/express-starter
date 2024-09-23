54cb31ac8a443ba761fbde9fd70000919b176068
Johnny Gérard <john@jgerard.dev>
2024-09-23 12:05:58 +0200

    fix: wrong path

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ const app = express();
 const HOST = "localhost";
 const PORT = 3000;
 
-app.use(express.static('../public'));
+app.use(express.static("public"));
 
 app.listen(PORT, HOST, () => {
   console.log(`Express server listening at http://${HOST}:${PORT}`);
