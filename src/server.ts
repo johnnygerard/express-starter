0e106a33d390ca26cea71ce938854dd98b47ee99
Johnny Gérard <john@jgerard.dev>
2024-09-23 10:46:25 +0200

    refactor: rename index.ts to server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 // This program is a quine (self-replicating program)
 import { readFile } from 'node:fs/promises';
 
-const data = await readFile('src/index.ts', { encoding: 'utf8' });
+const data = await readFile('src/server.ts', { encoding: 'utf8' });
 
 console.log(data);
